899ebacdd99d59b383667bb0521abfa5d619047a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-11-19 23:30:31 +0300

    Add Cli.java and mod. App.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scan.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        scan.close();
+    }
+}
