bfd0ac294ce0e756fb4ec44a756b2340bf1a98cb
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-11-24 19:36:33 +0300

    refactoring Engine.java and Even.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,110 +1,64 @@
 package hexlet.code;
 
-import hexlet.code.games.Even;
-import hexlet.code.games.Calc;
-import hexlet.code.games.GreatComDiv;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Prime;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public final class Engine {
+    private static String userName;
+    private static String gameQuestion;
+    private static String gameAnswer;
+    private static String gameRule;
+    private static boolean wasGreeting = false;
+    private static boolean isContinue;
+    private static final Scanner answer = new Scanner(System.in);
 
-    private static int number;
-    private static String partsOutput;
-    private static final int FIRST = 1;
-    private static final int SECOND = 2;
-    private static final int THIRD = 3;
-    private static final int FOURTH = 4;
-    private static final int FIFTH = 5;
-    private static final int SIXTH = 6;
-//    private static final Even EVEN = new Even();
-//    private static final Calc CALC = new Calc();
-//    private static final GreatComDiv GCD = new GreatComDiv();
-//    private static final Progression PROG = new Progression();
     public static void general() {
-        System.out.print("Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit\n"
-                + "Your choice: ");
-        Scanner scanner = new Scanner(System.in);
-        number = scanner.nextInt();
-        if (number == 0) {
-            return;
+
+        if (!wasGreeting) {
+            System.out.print("\n" + "Welcome to the Brain Games!\n" + "May I have your name? ");
+            userName = answer.nextLine();
+            System.out.println("Hello, " + userName + "!");
+            System.out.println(gameRule);
+            wasGreeting = true;
         }
-        Cli inputName = new Cli();
-        inputName.greet();
-        switch (number) {
-            case SECOND:
-                System.out.println(Even.getRuleEven());
-                break;
-            case THIRD:
-                System.out.println(Calc.getRuleCalc());
-                break;
-            case FOURTH:
-                System.out.println(GreatComDiv.getRuleGCD());
-                break;
-            case FIFTH:
-                System.out.println(Progression.getRuleProgression());
-                break;
-            case SIXTH:
-                System.out.println(Prime.getRulePrime());
-                break;
-            default:
-                break;
-        }
-        String answer;
-        int count = 0;
-        final int iterationCount = 3;
-        if (combinedOutputFromGame(number) != null) {
-            while (count < iterationCount) {
-                partsOutput = combinedOutputFromGame(number);
-                var parts = partsOutput.split("@");
-                System.out.println("Question: " + parts[0]);
-                System.out.print("Your answer: ");
-                answer = inputName.useScanner().nextLine();
-                if (!answer.equals(parts[1])) {
-                    System.out.println("'" + answer + "'"
-                            + " is wrong answer ;(. Correct answer was " + "'" + parts[1] + "'.\n"
-                            + "Let's try again, " + Cli.getUserName() + "!");
-                    break;
-                }
-                System.out.println("Correct!");
-                count++;
+            System.out.println("Question: " + gameQuestion);
+            System.out.print("Your answer: ");
+            String userAnswer;
+            userAnswer = answer.nextLine();
+            if (!gameAnswer.equals(userAnswer)) {
+                System.out.println("'" + userAnswer + "'"
+                        + " is wrong answer ;(. Correct answer was " + "'" + gameAnswer + "'.\n"
+                        + "Let's try again, " + userName + "!");
+                isContinue = false;
             }
-        }
-        if (count == iterationCount) {
-            System.out.println("Congratulations, " + Cli.getUserName() + "!");
-        }
-        inputName.useScanner().close();
-        scanner.close();
+            System.out.println("Correct!");
+            isContinue = true;
+
+    }
+    public static void setUserName(String userNameCurrent) {
+        userName = userNameCurrent;
+    }
+    public static boolean getExodus() {
+        return isContinue;
+    }
+    public static void setGameAnswer(String gameAnswerCurrent) {
+        gameAnswer = gameAnswerCurrent;
+    }
+    public static void setGameQuestion(String gameQuestionCurrent) {
+        gameQuestion = gameQuestionCurrent;
+    }
+    public static void setGameRule(String gameRuleCurrent) {
+         gameRule = gameRuleCurrent;
+    }
+    public static void congratulate() {
+        System.out.println("Congratulations, " + userName + "!");
+    }
+    public static void closeScanner() {
+        answer.close();
     }
 
-    public static String combinedOutputFromGame(int choiceNumber) {
-        if (choiceNumber == FIRST) {
-            return null;
-        } else if (choiceNumber == SECOND) {
-            Even even = new Even();
-            return even.outputFromEven();
-        } else if (choiceNumber == THIRD) {
-            Calc calc = new Calc();
-            return calc.outputFromCalculator();
-        } else if (choiceNumber == FOURTH) {
-            GreatComDiv gcd = new GreatComDiv();
-            return gcd.outputFromGreatComDiv();
-        } else if (choiceNumber == FIFTH) {
-            Progression prog = new Progression();
-            return prog.outputFromProgression();
-        } else if (choiceNumber == SIXTH) {
-            Prime prime = new Prime();
-            return prime.outputFromPrime();
-        } else {
-            return null;
-        }
-    }
+
 }
+
+
+
--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -2,23 +2,41 @@ package hexlet.code.games;
 
 
 
+import hexlet.code.Engine;
+
 import java.util.Random;
 
 public final class Even {
-    private final String yes = "yes";
-    private final String no = "no";
-    private int number;
-    private String answer;
-    private static final  String RULEEVEN = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-    public String outputFromEven() {
-        Random rand = new Random();
-        final int upperBound = 100;
-        number = rand.nextInt(upperBound); //[0;99]
-        answer = number % 2 == 0 ? yes : no;
-        return String.valueOf(number) + "@" + answer;
-    }
-    public static String getRuleEven() {
-        return RULEEVEN;
+    private static final String yes = "yes";
+    private static final String no = "no";
+    private static int number;
+    private static String answer;
+    private static final String RULEEVEN = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
+    public static void startGameEven() {
+        Engine.setGameRule(RULEEVEN);
+        boolean isContinue = true;
+        int count = 0;
+        final int iterationCount = 3;
+        while (count < iterationCount) {
+            Random rand = new Random();
+            final int upperBound = 100;
+            number = rand.nextInt(upperBound); //[0;99]
+            answer = number % 2 == 0 ? yes : no;
+            Engine.setGameQuestion(String.valueOf(number));
+            Engine.setGameAnswer(answer);
+            Engine.general();
+            isContinue = Engine.getExodus();
+            if (!isContinue) {
+                break;
+            }
+            count++;
+        }
+        Engine.closeScanner();
+        if (count == iterationCount) {
+            Engine.congratulate();
+        }
     }
 }
+
 
