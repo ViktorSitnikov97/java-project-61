99bd7429004212701410cfc5ffe19f5acc9a300f
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-11-24 14:27:37 +0300

    New stage - refactoring App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,53 @@
 package hexlet.code;
 
+import hexlet.code.games.*;
+
+import java.util.Scanner;
+
 public class App {
+    private static int selectedGame;
+    private static final int FIRST = 1;
+    private static final int SECOND = 2;
+    private static final int THIRD = 3;
+    private static final int FOURTH = 4;
+    private static final int FIFTH = 5;
+    private static final int SIXTH = 6;
     public static void main(String[] args) {
-        Engine.general();
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "3 - Calc\n"
+                + "4 - GCD\n"
+                + "5 - Progression\n"
+                + "6 - Prime\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        selectedGame = scanner.nextInt();
+        switch (selectedGame) {
+            case FIRST:
+                Cli.greet();
+                break;
+            case SECOND:
+//                startGameEven();
+                break;
+            case THIRD:
+//                startGameCalc();
+                break;
+            case FOURTH:
+                System.out.println(GreatComDiv.getRuleGCD());
+                break;
+            case FIFTH:
+                System.out.println(Progression.getRuleProgression());
+                break;
+            case SIXTH:
+                System.out.println(Prime.getRulePrime());
+                break;
+            default:
+                System.out.println("GoodBye!");
+                break;
+        }
+        scanner.close();
     }
+
 }
