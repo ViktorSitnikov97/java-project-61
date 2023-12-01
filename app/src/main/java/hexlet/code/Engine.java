00afb5473547b4c4537bb3b2848fd14f4234db19
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-12-02 01:02:28 +0300

    refactoring Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,12 +3,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public final class Engine {
-    private static final int INDEXQUESTION = 0;
-    private static final int INDEXANSWER = 1;
-    private static String gameRule;
+    private static final int INDEX_QUESTION = 0;
+    private static final int INDEX_ANSWER = 1;
 
-
-    public static void general(String result) {
+    public static void general(String[][] roundsData, String gameRule) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\n" + "Welcome to the Brain Games!\n" + "May I have your name? ");
         String userName;
@@ -16,31 +14,26 @@ public final class Engine {
         System.out.println("Hello, " + userName + "!");
         System.out.println(gameRule);
         int count = 0;
-        final int iterationCount = 3;
-        String[] arrayQuestionsWithAnswers = result.split("!"); // [question@answer],[question@answer]
-        while (count < iterationCount) {
-            var arrayGameQuestionAnswer = arrayQuestionsWithAnswers[count].split("@"); // [question],[answer]
-            System.out.println("Question: " + arrayGameQuestionAnswer[INDEXQUESTION]);
+        final int roundsCount = 3;
+        while (count < roundsCount) {
+            System.out.println("Question: " + roundsData[count][INDEX_QUESTION]);
             System.out.print("Your answer: ");
             String userAnswer;
             userAnswer = scanner.nextLine();
-            if (!arrayGameQuestionAnswer[INDEXANSWER].equals(userAnswer)) {
+            if (!roundsData[count][INDEX_ANSWER].equals(userAnswer)) {
                 System.out.println("'" + userAnswer + "'"
                         + " is wrong answer ;(. Correct answer was " + "'"
-                        + arrayGameQuestionAnswer[INDEXANSWER] + "'.\n"
+                        + roundsData[count][INDEX_ANSWER] + "'.\n"
                         + "Let's try again, " + userName + "!");
                 break;
             }
             System.out.println("Correct!");
             count++;
         }
-        if (count == iterationCount) {
+        if (count == roundsCount) {
             System.out.println("Congratulations, " + userName + "!");
         }
         scanner.close();
-    }
-    public static void setGameRule(String gameRuleCurrent) {
-        gameRule = gameRuleCurrent;
     }
 }
 
