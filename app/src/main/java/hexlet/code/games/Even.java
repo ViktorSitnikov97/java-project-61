2defc0f78a79f110a6fd88519f28238d4984202b
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-12-03 22:10:34 +0300

    refactoring Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,15 +6,15 @@ public final class Even {
     public static void startGameEven() {
         final String rule = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         String[][] roundsData = new String[Engine.ROUNDS][Engine.QUANTITY_DATA];
+        final int bottomBound = 1;
+        final int upperBound = 100;
         for (int i = 0; i < Engine.ROUNDS; i++) {
-            roundsData[i] = generateRoundDataEven();
+            int number = Utils.generateNumber(bottomBound, upperBound);
+            roundsData[i] = generateRoundDataEven(number);
         }
         Engine.general(roundsData, rule);
     }
-    public static String[] generateRoundDataEven() {
-        final int bottomBound = 1;
-        final int upperBound = 100;
-        int number = Utils.generateNumber(bottomBound, upperBound);
+    public static String[] generateRoundDataEven(int number) {
         String question = String.valueOf(number);
         String answer = number % 2 == 0 ? "yes" : "no";
         return new String[]{question, answer};
