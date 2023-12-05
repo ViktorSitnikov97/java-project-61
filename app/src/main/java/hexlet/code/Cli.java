0d67ef05a830f76a3be1453908b327f92104992a
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2023-12-05 23:44:43 +0300

    fix Engine.java and Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public final class Cli {
-    private static String userName;
 
     public static void greet() {
         Scanner userAnswer = new Scanner(System.in);
         System.out.print("\n" + "Welcome to the Brain Games!\n" + "May I have your name? ");
-        userName = userAnswer.nextLine();
+        String userName = userAnswer.nextLine();
         System.out.println("Hello, " + userName + "!");
         userAnswer.close();
     }
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,7 +8,7 @@ public final class Engine {
     private static final int INDEX_QUESTION = 0;
     private static final int INDEX_ANSWER = 1;
 
-    public static String general(String[][] roundsData, String gameRule) {
+    public static void general(String[][] roundsData, String gameRule) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("\n" + "Welcome to the Brain Games!\n" + "May I have your name? ");
         String userName = scanner.nextLine();
@@ -20,15 +20,16 @@ public final class Engine {
             String userAnswer = scanner.nextLine();
             if (!data[INDEX_ANSWER].equals(userAnswer)) {
                 scanner.close();
-                return "'" + userAnswer + "'"
+                System.out.println("'" + userAnswer + "'"
                         + " is wrong answer ;(. Correct answer was " + "'"
                         + data[INDEX_ANSWER] + "'.\n"
-                        + "Let's try again, " + userName + "!";
+                        + "Let's try again, " + userName + "!");
+                return;
             }
             System.out.println("Correct!");
         }
+        System.out.println("Congratulations, " + userName + "!");
         scanner.close();
-        return "Congratulations, " + userName + "!";
     }
 }
 
